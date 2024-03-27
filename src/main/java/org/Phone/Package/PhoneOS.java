ecde523c1df7e1446f8508761b28228e42a45a65
Alex Haraga <haraga.alexandru@gmail.com>
2024-03-27 09:28:59 +0200

    General Structure and Ideas

--- src/main/java/org/Phone/Package/PhoneOS.java
+++ src/main/java/org/Phone/Package/PhoneOS.java
@@ -1,10 +1,9 @@
 package org.Phone.Package;
 
-public interface PhoneOS {
+interface PhoneOS {
     public void createContact();
     public void viewContacts();
     public void sendMessage();
     public void callContact();
     public void viewHistory();
-
 }
